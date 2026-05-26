6a8dbefa84c0bbcd2fe2c07df37034c67dae752a
Julian Bustillos <bustillosjulian@gmail.com>
2026-05-26 09:08:54 +0200

    Clean tracked ignored files

--- src/DateParser.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "FileData.hpp"
-#include <QByteArray>
-
-namespace DateParser {
-bool fromJPGBuffer(const QByteArray &buffer, Date &date);
-bool fromMP4Buffer(const QByteArray &buffer, Date &date);
-bool fromFileName(const std::string &fileName, Date &date);
-} // namespace DateParser
